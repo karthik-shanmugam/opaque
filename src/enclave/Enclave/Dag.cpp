38017e0f81bb23b074ebdc1570eaf57204f1bf5f
Karthik Shanmugam <karthiks@berkeley.edu>
2017-09-14 01:34:02 -0700

    save me

--- src/enclave/Enclave/Dag.cpp
+++ src/enclave/Enclave/Dag.cpp
@@ -30,7 +30,7 @@ void get_dependencies_for_node(
     ocall_malloc(sizeof(int) * 2, (uint8_t **) output_tokens);
     *output_tokens_length = target->dependencies()->size();
     *output_tokens[0] = 1996 + target->dependencies()->size();
-    *output_tokens[1] = target->dependencies()->Get(1)->token();
+    *output_tokens[1] = target->dependencies()->Get(0)->token();
     return;
 
     // ocall_malloc(target->dependencies()->size() * sizeof(int), (uint8_t **) output_tokens);
