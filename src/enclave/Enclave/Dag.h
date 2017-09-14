469db921ac3279d4eddb1a1941a1a8773a5f216b
Karthik Shanmugam <karthiks@berkeley.edu>
2017-09-13 22:33:59 -0700

    more bug fixes

--- src/enclave/Enclave/Dag.h
+++ src/enclave/Enclave/Dag.h
@@ -5,7 +5,7 @@
 #define DAG_H
 
 void get_dependencies_for_node(
-  uint8_t *dag_ptr, size_t dag_length,
+  uint8_t *dag_ptr, size_t dag_length, int node,
   uint32_t **output_tokens, size_t *output_tokens_length);
 
 #endif // DAG_H
