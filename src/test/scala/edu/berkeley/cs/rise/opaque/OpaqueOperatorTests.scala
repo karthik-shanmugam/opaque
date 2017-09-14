e048553a79957c39ce67924dd47236f246e91e51
Karthik Shanmugam <karthiks@berkeley.edu>
2017-09-14 01:14:56 -0700

    yay

--- src/test/scala/edu/berkeley/cs/rise/opaque/OpaqueOperatorTests.scala
+++ src/test/scala/edu/berkeley/cs/rise/opaque/OpaqueOperatorTests.scala
@@ -335,6 +335,7 @@ trait OpaqueOperatorTests extends FunSuite with BeforeAndAfterAll { self =>
 
     val res = enclave.DependenciesForNode(eid, builder.sizedByteArray(), target.token)
     println("begin dag test ouput ------------")
+    println(DAGUtils.DAGtoString(dag))
     println(res.toList)
     println(target.dependencies.map{_.token})
     println("end dag test ouput --------------")
